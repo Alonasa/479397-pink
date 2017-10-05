9fe5e9d06f374a2921bdd93dfc39c778281f4c5d
Alonesa <alenakrasnov@mail.ru>
2017-10-06 01:51:16 +0300

    page header

---
+++ js/script.js
@@ -0,0 +1,14 @@
+var navMain = document.querySelector('.site-list');
+      var navToggle = document.querySelector('.site-list__toggle');
+
+      navMain.classList.remove('site-list--nojs');
+
+      navToggle.addEventListener('click', function() {
+        if (navMain.classList.contains('site-list--closed')) {
+          navMain.classList.remove('site-list--closed');
+          navMain.classList.add('site-list--opened');
+        } else {
+          navMain.classList.add('site-list--closed');
+          navMain.classList.remove('site-list--opened');
+        }
+      });
